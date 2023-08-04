a562497037b5ba6be9a2f16b66ecc225ea1ca741
Erkan <erkankolakan@gmail.com>
2023-08-04 21:20:44 +0300

    SSR&CSR olayları öğrenildi

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,9 @@ const Layout = ({children}) => {
   return (
     <html lang='en'>
         <body>
+          <header>BU KISIM HEADER</header>
             {children}
+          <footer>BU KISIM FOOTER</footer>
         </body>
     </html>
   )
@@ -13,3 +15,10 @@ const Layout = ({children}) => {
 export default Layout
 
 //bu kısım tamamiyle sarmalama işlemlerini yapabilmesi için kullanılan bir sayfa
+/*
+
+Next.js'te layout sayfaları, web uygulamanızdaki tüm sayfalar için ortak bir görünüm ve his sağlamak için kullanılır. Layout sayfaları, sayfa başlıkları, altbilgileri, kenar çubukları ve diğer ortak öğeleri içerebilir. Layout sayfaları, sayfalarınızın daha tutarlı ve profesyonel görünmesini sağlar.
+
+ Bizim react da headerı footerı roouter üzerine kodyuğumuz düzen burada daha kolay. Direk sayfayı çağırak body içerisine yazmamız yeterli.
+
+*/
---
+++ app/trial.jsx
@@ -0,0 +1,18 @@
+"use client"
+import React from 'react'
+
+
+const Trial = () => {
+    console.log("trial")
+  return (
+    <div>
+ 
+    </div>
+  )
+}
+
+export default Trial
+//arık burda yazdırığın parametreler web sayfasında console ekranında gözükücek.
+//Ama bizim en üste bu sayfanın "use client" olduğunu söylememiz gerekiyor.
+//Hangi sayfada gözükmesini istiyorsan o sayfaya bu sayfayı şmport etmen gerekir.
+
